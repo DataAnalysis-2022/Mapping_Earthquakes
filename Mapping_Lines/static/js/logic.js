0575ed5e596a5c76810dfbc4edbc48c5bec2ee68
DataAnalysis-2022 <106888594+DataAnalysis-2022@users.noreply.github.com>
2022-09-04 10:05:51 -0400

    New Files

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,16 +2,42 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);  //different ways
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      36.1733, -120.1794
-    ],
-    zoom: 5
-  });
+// let map = L.map("mapid", {
+//     center: [
+//       36.1733, -120.1794
+//     ],
+//     zoom: 5
+//   });
+
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
+
 
   // Coordinates for each point to be used in the line.
 let line = [
